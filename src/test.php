aaee29adf8187276e90eba59a924469eaaa4b536
12blackpearls <12blackpearls@gmail.com>
2016-08-08 11:00:33 +0700

    Add Stem & Stop Words Removal

--- src/test.php
+++ src/test.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 use Sastrawi\Stemmer\StemmerFactory;
+use Sastrawi\StopWordRemover\StopWordRemoverFactory;
+
 //use Wamania\Snowball\French;
 $stemmerFactory = new StemmerFactory();
 $stemmer  = $stemmerFactory->createStemmer();
@@ -13,7 +15,11 @@ $output   = $stemmer->stem($sentence);
 //echo $output . "\n";
 // ekonomi indonesia sedang dalam tumbuh yang bangga
 
-echo $stemmer->stem('Memakan') . "\n";
-// mereka tiru
+//stopwordremoval
+$stopwFactory = new StopWordRemoverFactory();
+$stopw = $stopwFactory->createStopWordRemover();
+
+$output2 = $stopw->remove($output);
+echo $output2;
 
 ?>
