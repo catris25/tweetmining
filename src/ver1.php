572be7ec1662bfa69d8c20cb514f60e37b7304cb
12blackpearls <12blackpearls@gmail.com>
2016-08-16 16:56:46 +0700

    Add Maps

--- src/ver1.php
+++ src/ver1.php
@@ -1,4 +1,5 @@
 <?php 
+    namespace TweetMining;
 	require_once __DIR__ . '/../vendor/autoload.php';
 	use Sastrawi\Stemmer\StemmerFactory;
 	use Sastrawi\StopWordRemover\StopWordRemoverFactory; 
@@ -11,7 +12,7 @@
 
     //load training set file
     $training = array();
-    if (($handle = fopen("training.csv", "r")) !== FALSE) {
+    if (($handle = fopen("tweets_training_v1.csv", "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $training[] = $data;
         }
@@ -20,7 +21,7 @@
 
     //load testing set file
     $testing = array();
-    if (($handle = fopen("testing.csv", "r")) !== FALSE) {
+    if (($handle = fopen("tweets_testing_v1.csv", "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $testing[] = $data;
         }
@@ -36,14 +37,14 @@
 	$stopw = $stopwFactory->createStopWordRemover();
 
     //stem and remove stopwords training set 
-	for($x = 0; $x < 80; $x++) {
+	for($x = 0; $x < 73; $x++) {
 		$training[$x][1] = $stemmer->stem($training[$x][1]);
 		$training[$x][1] = $stopw->remove($training[$x][1]);
 		//echo $twoDarray[$x][1]."<br>";
 	}
 
     //stem and remove stopwords testing set 
-	for($x = 0; $x < 14; $x++) {
+	for($x = 0; $x < 10; $x++) {
 		$training[$x][1] = $stemmer->stem($training[$x][1]);
 		$training[$x][1] = $stopw->remove($training[$x][1]);
 		//echo $twoDarray[$x][1]."<br>";
@@ -84,12 +85,80 @@
         );
         if ($prediction==$d[2]) {
             $correct ++;
-            printf($counter, " ");
-        }
-            
-        $counter ++;
-    }
-    
-    printf("Accuracy: %.2f\n", 100*$correct / count($testing));
+            //printf($counter, " ");
+            if($d[2] == 'terkait') {
+                print $d[1] . "<br>";
 
+                //convert string to unigram, bigram, trigram
+                $bigram = new Bigram();
+                $unibitri = $bigram->tokenize($d[1]);
+
+                foreach($unibitri as $value) {
+                    //search in maps
+                    $data_arr = geocode($value);
+    
+                    // if able to geocode the address
+                    if($data_arr){
+                        
+                        $latitude = $data_arr[0];
+                        $longitude = $data_arr[1];
+                        $formatted_address = $data_arr[2];
+                        print "found in : " . $value . "<br>" . $latitude . "<br>" . $longitude . "<br>";
+                    }
+                }
+                print "<br><br>";
+            }
+            
+            $counter ++;
+        }
+    }
+
+    //printf("Accuracy: %.2f\n", 100*$correct / count($testing));
+    
+    //function to search in maps
+    function geocode($address){
+ 
+        // url encode the address
+        $address = urlencode($address);
+        
+        // google map geocode api url
+        $url = "http://maps.google.com/maps/api/geocode/json?address={$address}";
+    
+        // get the json response
+        $resp_json = file_get_contents($url);
+        
+        // decode the json
+        $resp = json_decode($resp_json, true);
+    
+        // response status will be 'OK', if able to geocode given address 
+        if($resp['status']=='OK'){
+    
+            // get the important data
+            $lati = $resp['results'][0]['geometry']['location']['lat'];
+            $longi = $resp['results'][0]['geometry']['location']['lng'];
+            $formatted_address = $resp['results'][0]['formatted_address'];
+            
+            // verify if data is complete
+            if($lati && $longi && $formatted_address){
+            
+                // put the data in the array
+                $data_arr = array();            
+                
+                array_push(
+                    $data_arr, 
+                        $lati, 
+                        $longi, 
+                        $formatted_address
+                    );
+                
+                return $data_arr;
+                
+            }else{
+                return false;
+            }
+            
+        }else{
+            return false;
+        }
+    }
 ?>
